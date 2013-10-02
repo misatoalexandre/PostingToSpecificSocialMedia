6fdd6feb4b6a1982b7326dbfbc31453d5e255c24
misatoalexandre <misatotina.alexandre@gmail.com>
2013-10-02 12:06:13 -0400

    Initial Commit

---
+++ PostingToSpecificSocialMedia/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PostingToSpecificSocialMedia
+//
+//  Created by Misato Tina Alexandre on 10/2/13.
+//  Copyright (c) 2013 Misato Tina Alexandre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PostingToSpecificSocialMedia/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PostingToSpecificSocialMedia
+//
+//  Created by Misato Tina Alexandre on 10/2/13.
+//  Copyright (c) 2013 Misato Tina Alexandre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
